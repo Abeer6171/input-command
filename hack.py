75cdbc7924fce2403ba3531dd14c2472742c9edb
Abeer6171 <champchess984@gmail.com>
2025-01-17 20:51:06 +0530

    Add files via upload

---
+++ hack.py
@@ -0,0 +1,17 @@
+print("arthematical operators in phython")
+print("ADDITION")
+a=10
+b=10
+print (a+b)
+Print("substraction")
+c=21
+d=32
+print(c-d)
+print("multipilication")
+e=2
+f=4
+print(e*f) 
+print("division")
+g=10
+h=2
+print(g/h)
